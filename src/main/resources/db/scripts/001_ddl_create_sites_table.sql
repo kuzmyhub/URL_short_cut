9325a86d6452fe91b6c6660251ca5073938fe2f4
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-12-09 08:12:51 +0300

    2. Сервис - UrlShortCut [#13799] - в скрипты таблиц добавлены уникальные ограничения и комментарии

--- src/main/resources/db/scripts/001_ddl_create_sites_table.sql
+++ src/main/resources/db/scripts/001_ddl_create_sites_table.sql
@@ -1,7 +1,13 @@
 CREATE TABLE IF NOT EXISTS sites (
 id SERIAL PRIMARY KEY,
-name VARCHAR,
-login VARCHAR,
+name VARCHAR UNIQUE,
+login VARCHAR UNIQUE,
 password VARCHAR,
 registration BOOLEAN
 );
+
+COMMENT ON TABLE sites IS 'Данные зарегестрированных сайтов';
+COMMENT ON COLUMN sites.id IS 'Идентификатор сайта';
+COMMENT ON COLUMN sites.name IS 'Доменное имя сайта HTTP://DOMAIN.DOMAIN';
+COMMENT ON COLUMN sites.login IS 'Логин сайта';
+COMMENT ON COLUMN sites.password IS 'Пароль сайта';
--- src/main/resources/db/scripts/002_ddl_create_links_table.sql
+++ src/main/resources/db/scripts/002_ddl_create_links_table.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS links (
 id SERIAL PRIMARY KEY,
-url VARCHAR,
-code VARCHAR,
+url VARCHAR UNIQUE,
+code VARCHAR UNIQUE,
 total INT,
 site_id INT references sites(id)
 );
+
+COMMENT ON TABLE links IS 'Данные ссылок зарегистрированных сайтов';
+COMMENT ON COLUMN links.id IS 'Идентификатор ссылки';
+COMMENT ON COLUMN links.url IS 'Ссылка';
+COMMENT ON COLUMN links.code IS 'Код ассоциированный со ссылкой';
+COMMENT ON COLUMN links.total IS 'Количество вызовов ссылки';
+COMMENT ON COLUMN links.site_id IS 'Идентификатор зарегистрированного сайта';
