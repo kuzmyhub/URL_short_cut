501a52287d7d6857d77481eafcd38a9acb0fe4f8
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2023-01-29 19:01:12 +0300

    Liquibase fix

---
+++ src/test/java/ru/job4j/urlshortcut/contorller/LinkControllerTest.java
@@ -0,0 +1,10 @@
+package ru.job4j.urlshortcut.contorller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LinkControllerTest {
+    @Test
+    public void test() {
+    }
+}
